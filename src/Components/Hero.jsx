18b7123482695bd385d46d84b2c618ec0a1e7200
xreotrace <marufbillah.dev@gmail.com>
2026-03-24 22:28:18 +0600

    added navbar and hero

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,26 @@
+import heroLogo from "../assets/banner-main.png";
+
+const Hero = () => {
+  return (
+    <div className="max-w-330 mx-auto px-2">
+      <div className="rounded-3xl py-16 text-center flex flex-col gap-6 items-center bg-[#131313] bg-[url(./bg-shadow.png)] bg-cover bg-no-repeat bg-center mb-22">
+        <img src={heroLogo} alt="" />
+        <div className="hero-contents space-y-6">
+          <h1 className="font-bold text-[40px] text-white">
+            Assemble Your Ultimate Dream 11 Cricket Team
+          </h1>
+          <p className="font-inter text-2xl font-medium text-white/70">
+            Beyond Boundaries Beyond Limits
+          </p>
+          <button className="rounded-2xl p-2 border-2 border-[#E7FE29] cursor-pointer">
+            <div className="rounded-lg p-3.5 bg-[#E7FE29] font-bold text-neutral">
+              Claim Free Credit
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,77 @@
+import dollarIcon from "../assets/dollar.png";
+import navLogo from "../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-base-100 shadow-sm mb-6 sticky">
+      <div className="navbar max-w-330 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-6 w-52 p-2 shadow"
+            >
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Fixture</a>
+              </li>
+              <li>
+                <a href="">Teams</a>
+              </li>
+              <li>
+                <a href="">Schedules</a>
+              </li>
+            </ul>
+          </div>
+          <a href="/">
+            <img src={navLogo} alt="" />
+          </a>
+        </div>
+        <div className="navbar-end gap-8">
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Fixture</a>
+              </li>
+              <li>
+                <a href="">Teams</a>
+              </li>
+              <li>
+                <a href="">Schedules</a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex gap-2.5 px-5 py-4 rounded-xl border border-[#131313]/10 cursor-pointer hover:bg-[#131313]/10">
+            <p className="font-semibold">
+              <span>0</span> Coins
+            </p>
+            <img src={dollarIcon} alt="" className="h-5 w-5" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
